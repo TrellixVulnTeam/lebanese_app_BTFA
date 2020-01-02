f712e20f769778f33e62cdb2ccdf90558ece792a
nathanbowley98 <nathanbowley98@live.com>
2020-01-01 21:31:03 -0400

    starting working on the app got to figure out how to add in the ability to have my app also return my defs

--- venv/main.py
+++ venv/main.py
@@ -1,28 +1,42 @@
 """program for translating arabic characters into roman arabic script"""
 
+#importing pydub for audio
 from pydub import AudioSegment
 from pydub.playback import play
+
+#importing lebanese module stuff
 from lebanese import breakdown_lebanese
 from breakdown_lebanese import arabic_break_down
 from lebanese import lebanese_to_roman
 from lebanese_to_roman import arabic_to_roman_replace
 
+#importing kivy stuff
 import kivy
 kivy.require('1.11.1') # replace with your current kivy version !
 
 from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
 
 
+class Translator(GridLayout):
+
+    def __init__(self, **kwargs):
+        super(Translator, self).__init__(**kwargs)
+        self.cols = 2
+        self.add_widget(Label(text="Input arabic word here to have it converted: "))
+        self.test = TextInput(password=False, multiline=True,)
+        self.add_widget(self.test)
+# https://kivy.org/doc/stable/guide/basic.html#create-an-application
 class MyApp(App):
 
-    #def build(self):
+    def build(self):
+        return Translator()
 
-     #   return Label(text='Hello world')
-
-    layout = BoxLayout(padding=10)
+"""    layout = BoxLayout(padding=10)
     button = Button(text='My first button')
-    layout.add_widget(button)
+    layout.add_widget(button)"""
 
 if __name__ == '__main__':
     MyApp().run()
