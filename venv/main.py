dd186c78d2701b7a9274a8747804022038dab488
nathanbowley98 <nathanbowley98@live.com>
2020-01-02 23:25:29 -0400

    this is my lebanese arabic to english app, it has some bugs and I don't know how to fix them made by nathanbowley98 on github

--- venv/main.py
+++ venv/main.py
@@ -1,5 +1,7 @@
 """program for translating arabic characters into roman arabic script"""
 
+import os
+
 #importing pydub for audio
 from pydub import AudioSegment
 from pydub.playback import play
@@ -13,26 +15,59 @@ from lebanese_to_roman import arabic_to_roman_replace
 #importing kivy stuff
 import kivy
 kivy.require('1.11.1') # replace with your current kivy version !
-
+#from kivy.config import Config
+#Config.set('modules', 'screen', 'phone_samsung_galaxy_s5')
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
+from kivy.lang import Builder
+from kivy.graphics import Color, Rectangle
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.image import AsyncImage
+from kivy.uix.button import Button
+from kivy.uix.widget import Widget
 
 
-class Translator(GridLayout):
+class RootWidget(BoxLayout):
+    pass
+
+class Background(Widget):
+    pass
+
+class TranslatorLayout(FloatLayout):
 
     def __init__(self, **kwargs):
-        super(Translator, self).__init__(**kwargs)
-        self.cols = 2
-        self.add_widget(Label(text="Input arabic word here to have it converted: "))
-        self.test = TextInput(password=False, multiline=True,)
-        self.add_widget(self.test)
+        super(TranslatorLayout, self).__init__(**kwargs)
+
+        with self.canvas:
+            Rectangle(source='C:/Users/natha/PycharmProjects/translator/venv/images/background.png', pos=self.pos, size=self.size)
+
+        #self.cols = 2
+        #self.add_widget(Label(text="Input arabic word here to have it converted: "))
+        #self.test = TextInput(password=False, multiline=True,)
+        #self.add_widget(self.test)
+
+      #  "C:/Users/natha/PycharmProjects/translator/venv/images"
+
 # https://kivy.org/doc/stable/guide/basic.html#create-an-application
 class MyApp(App):
 
     def build(self):
-        return Translator()
+
+        root = RootWidget()
+        t = TranslatorLayout()
+        root.add_widget(t)
+        #root.add_widget(AsyncImage(
+         #       source="C:/Users/natha/PycharmProjects/translator/venv/images/background.png",
+           #     size_hint= (1, .5),
+          #      pos_hint={'center_x':.5, 'center_y':.5}))
+
+        #root.add_widget(t)
+        return root
+        #root.add_widget(AsyncImage("C:/Users/natha/PycharmProjects/translator/venv/images") )
+        
 
 """    layout = BoxLayout(padding=10)
     button = Button(text='My first button')
@@ -51,5 +86,6 @@ def main():
     print(test_word, sep=" ")
     breakdown_lebanese.arabic_break_down(test_word)
     lebanese_to_roman.arabic_to_roman_replace(test_word)
+    print(os.path.dirname(__file__))
 if __name__ == '__main__':
     main()
